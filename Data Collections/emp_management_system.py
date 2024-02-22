a13160693896caa67209badc76386ed9983f8530
sqb-codes <150049400+sqb-codes@users.noreply.github.com>
2024-02-22 09:23:11 +0530

    Add files via upload

--- Data Collections/emp_management_system.py
+++ Data Collections/emp_management_system.py
@@ -1,7 +1,12 @@
 #CRUD (Create Read Update Delete) Operations
 # Emp Management System
 
-employees = []
+employees = [
+    {"emp_id" : 1001, "emp_name" : "Ram", "emp_dept" : "IT", "salary" : 45000},
+    {"emp_id" : 1002, "emp_name" : "Akash", "emp_dept" : "IT", "salary" : 25000},
+    {"emp_id" : 1003, "emp_name" : "Mohit", "emp_dept" : "HR", "salary" : 50000},
+    {"emp_id" : 1004, "emp_name" : "Vaibhav", "emp_dept" : "IT", "salary" : 55000},
+    ]
 
 print("""
 Login as:
@@ -33,6 +38,12 @@ if ch == 1:
             salary = input("Enter employee salary : ")
             emp = {"emp_id":emp_id, "emp_name":emp_name, "emp_dept":emp_dept,"salary":salary}
             employees.append(emp)
+        elif admin_ch == 2:
+            emp_id = int(input("Enter Employee ID you want to delete : "))
+
+        elif admin_ch == 3:
+            emp_id = int(input("Enter Employee ID you want to update : "))
+        
         elif admin_ch == 4:
             print("All Employees Data")
             for i in range(len(employees)):
@@ -45,3 +56,17 @@ if ch == 1:
             login_status = False
 else:
     print("Emp Dashboard")
+    emp_id = int(input("Enter Emp ID : "))
+    for i in range(len(employees)):
+        if employees[i]["emp_id"] == emp_id:
+            print("Login Success...")
+            print("Emp_ID :",employees[i]["emp_id"])
+            print("Emp_Name :",employees[i]["emp_name"])
+            print("Emp_Department :",employees[i]["emp_dept"])
+            print("Emp_Salary :",employees[i]["salary"])
+            break
+    else:
+        print("Login Failed...")
+
+    
+        
